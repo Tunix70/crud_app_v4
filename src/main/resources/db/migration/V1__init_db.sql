8092b31ddcde5c817e5a045e38716411cf3a88f0
worker <pestekhin70@yandex.ru>
2021-04-15 17:32:18 +0700

    откатился к первой версии

--- src/main/resources/db/migration/V1__init_db.sql
+++ src/main/resources/db/migration/V1__init_db.sql
@@ -1,20 +1,24 @@
-CREATE TABLE IF NOT EXISTS post
-(post_id SERIAL NOT NULL PRIMARY KEY,
-content TEXT,
-created TIMESTAMP NOT NULL,
-UPDATED TIMESTAMP NOT NULL,
-post_status VARCHAR(10)
-);
-
 CREATE TABLE IF NOT EXISTS region
-(region_id SERIAL NOT NULL  PRIMARY KEY,
-name VARCHAR(10) NOT NULL UNIQUE
+(region_id SERIAL PRIMARY KEY,
+ name VARCHAR(10) NOT NULL UNIQUE
 );
 
 CREATE TABLE IF NOT EXISTS writer
-(writer_id SERIAL NOT NULL PRIMARY KEY,
-firstname VARCHAR (30),
-lastname VARCHAR (30),
-post_id INTEGER,
-region_id INTEGER
+(writer_id SERIAL PRIMARY KEY,
+ firstname VARCHAR (30),
+ lastname VARCHAR (30),
+ region_id INTEGER
 );
+
+CREATE TABLE IF NOT EXISTS post
+(post_id SERIAL PRIMARY KEY,
+ content TEXT,
+ created TIMESTAMP NOT NULL,
+ UPDATED TIMESTAMP NOT NULL,
+ post_status VARCHAR(10),
+ writer_id INTEGER,
+ FOREIGN KEY (writer_id) REFERENCES writer (writer_id)
+);
+
+
+
